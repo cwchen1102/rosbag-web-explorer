d0130fb77921ec1e89adc87d96c0771cd3e5c93e
cwchen1102 <chingwen.tech@gmail.com>
2023-09-21 17:30:07 +0800

    show rosbag details on website

--- backend/app/metadata.py
+++ backend/app/metadata.py
@@ -1,10 +1,12 @@
 import rosbag
 import subprocess
 import time
+import os
+from django.conf import settings
 
 class MetaData:
     def __init__(self, file_path):
-        self.file_path = file_path
+        self.file_path = os.path.dirname(settings.MEDIA_ROOT) + file_path
         self.meta_data = dict()
     def read(self):
         bag = rosbag.Bag(self.file_path, 'r')
