5eb7e4a21c84b0dc928b3a5e7633f2fa055cf7eb
cwchen1102 <chingwen.tech@gmail.com>
2023-09-27 18:56:49 +0800

    display topic info

--- backend/app/metadata.py
+++ backend/app/metadata.py
@@ -14,4 +14,14 @@ class MetaData:
         self.meta_data["start_time"] = bag.get_start_time()
         self.meta_data["end_time"] = bag.get_end_time()
         self.meta_data["size"] = bag.size
+        topics_info = []
+        for topic, info in bag.get_type_and_topic_info().topics.items():
+            topic_info = {"topic" : topic,
+                          "msg_type" : info.msg_type,
+                          "message_count" : info.message_count,
+                          "connections" : info.connections,
+                          "frequency" : info.frequency}
+            topics_info.append(topic_info)
+        self.meta_data["topics_info"] = topics_info
+        print(self.meta_data)
         return self.meta_data
